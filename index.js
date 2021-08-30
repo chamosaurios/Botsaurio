0d4ed8d10a6b8bc3190dda118d5862f9972df990
Chamosaurios <89287610+chamosaurios@users.noreply.github.com>
2021-08-30 15:49:35 +0000

    agregado el uso de un símbolo identificador

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const Discord = require('discord.js.old')
 const client = new Discord.Client()
+const config = require("./config.json"); //referencia el archivo config.json
 
+//agregación del objeto prefix
+const prefix = config.prefix; 
+
+//muestra en consola si se logro la conexión
 client.on("ready", ()=>{
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
-client.on("message", msg => {
-  if(msg.content === "#ping"){
-    msg.reply("pong")
+client.on("message", (msg) => {
+
+  //evita contestar mensajes de bots
+  if(message.author.bot) return;
+
+  if(msg.content === `${prefix}ping` ){
+    msg.channel.send("pong 🏓!!")
   }
+
+  if(msg.content === `${prefix}help` ){
+    msg.channel.send("En desarrollo disculpa 😅")
+  }
+
+
 })
 
-//client.login(process.env["TOKEN"])
+
 client.login(process.env["TOKEN"])
